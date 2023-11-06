668c8138feba0c8c49754b9d4425d03e70d55e5e
Raj <SoftDevRaj77@gmail.com>
2023-11-06 15:55:33 -0500

    edited index.js

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
+// Load .env file to process so we can access environment variables
 require('dotenv').config();
-const express = require('express');
-const sequelize = require('./config/database');
 
+// Import the express package which is used to create our HTTP server
 const express = require('express');
+
+// Import Sequelize configured instance to use for database operations
+const sequelize = require('./config/database'); // Adjust the path as necessary for your project
+
+// Create an instance of express which is our server
 const app = express();
+
+// Define a port number from the environment variable or default to 3000
 const PORT = process.env.PORT || 3000;
 
+// Setup a response for the root route to check if our server is running
 app.get('/', (req, res) => {
-  res.send('Backend Bazaar is running!');
+  // Send a response when this route is accessed
+  res.send('Welcome to Backend Bazaar!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+// Connect to our database using Sequelize
+sequelize.authenticate()
+  .then(() => {
+    // If connection is successful, log the success message
+    console.log('Connection has been established successfully.');
+
+    // Start the server and listen on the defined PORT
+    app.listen(PORT, () => {
+      // Log the message to the console after the server starts listening
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    // If there is any error connecting to the database, log the error
+    console.error('Unable to connect to the database:', err);
+  });
+
